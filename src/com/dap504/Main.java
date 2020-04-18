6249df62ce0ec43d004b6ded368b71197cb45da0
Roger Holden <r.holden@chi.ac.uk>
2020-04-18 08:24:06 +0100

    bike class added

--- src/com/dap504/Main.java
+++ src/com/dap504/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Just printing a iine");
+        System.out.println("Just printing a second iine");
     }
 }
