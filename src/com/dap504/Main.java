cdf0d4db1e1c99d57e8c8c51e1465cb031f973ed
Roger Holden <r.holden@chi.ac.uk>
2020-04-18 08:03:50 +0100

    Initial commit

---
+++ src/com/dap504/Main.java
@@ -0,0 +1,11 @@
+package com.dap504;
+
+// this is just a test project that can be deleted. it is to test creating a project then
+// sending / creating a new git repo (ie one not already st up)
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
